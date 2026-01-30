03b2bb8fa5ac887bfcce3e5b29bedf823f659c03
jason910727 <131846167+jason910727@users.noreply.github.com>
2026-01-30 15:16:30 +0800

    Update sketch_jan30a.ino

--- sketch_jan30a.ino
+++ sketch_jan30a.ino
@@ -1,9 +1,9 @@
-const int leds[] = {4, 5, 18, 19, 21, 22, 23, 25};
+const int leds[] = {4, 5, 18, 19, 21, 22, 23, 25};   // 共8個LED 占八個腳位 這裡的腳位代表ESP32的腳位
 const int N = sizeof(leds) / sizeof(leds[0]);
 
 int delayMs = 120;
 
-void setup(){
+void setup(){                          // <-設定初始狀態 (全滅)
   for(int i = 0; i< N; i++){
     pinMode(leds[i], OUTPUT);
     digitalWrite(leds[i], LOW);
